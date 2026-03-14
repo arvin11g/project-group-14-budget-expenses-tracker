ed9d162cdece153aff8ca3ab3706ff6e7237b216
iyinb <iyinb@my.yorku.ca>
2026-03-13 23:56:49 -0400

    final changes to files

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -10,7 +10,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="app-container">
+   <div className="app-container">
       <Sidebar />
 
       <div className="main-content">
